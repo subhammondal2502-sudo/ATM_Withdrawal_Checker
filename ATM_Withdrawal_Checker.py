fef0192270b71647cfdfae5634b690da0019d320
Subham Mondal <subhammondal2502@gmail.com>
2026-09-12 13:42:53 +0530

    Add files via upload

---
+++ ATM_Withdrawal_Checker.py
@@ -0,0 +1,4 @@
+#ATM Withdrawal Checker..........
+balance=float(input("enter your balance : "))
+withdraw_amount=float(input("enter your withdraw_amount : "))
+Remaining_balance=balance-withdraw_amount
