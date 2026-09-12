6675f25732aff5d39b2bfe39ac13f8ebd6b1ff9a
Subham Mondal <subhammondal2502@gmail.com>
2026-09-12 13:49:10 +0530

    Add invalid balance check

--- ATM_Withdrawal_Checker.py
+++ ATM_Withdrawal_Checker.py
@@ -4,3 +4,5 @@ withdraw_amount=float(input("enter your withdraw_amount : "))
 Remaining_balance=balance-withdraw_amount
 print("balance : ", balance)
 print("withdraw_amount : ", withdraw_amount)
+if balance < 0:
+    print("Invalid balance")
