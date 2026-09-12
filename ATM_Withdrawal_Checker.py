e1fd3fee7eed48337fc3e527ea0935849b62f76a
Subham Mondal <subhammondal2502@gmail.com>
2026-09-12 13:45:47 +0530

    Display balance and withdrawal amount

--- ATM_Withdrawal_Checker.py
+++ ATM_Withdrawal_Checker.py
@@ -2,3 +2,5 @@
 balance=float(input("enter your balance : "))
 withdraw_amount=float(input("enter your withdraw_amount : "))
 Remaining_balance=balance-withdraw_amount
+print("balance : ", balance)
+print("withdraw_amount : ", withdraw_amount)
