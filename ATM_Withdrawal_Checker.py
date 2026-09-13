fdc3b1e1d1d9f8b2410981b86e8e7c7b036ba2f6
Subham Mondal <subhammondal2502@gmail.com>
2026-09-13 08:01:15 +0530

    Update ATM_Withdrawal_Checker.py

--- ATM_Withdrawal_Checker.py
+++ ATM_Withdrawal_Checker.py
@@ -6,12 +6,9 @@ print("balance : ", balance)
 print("withdraw_amount : ", withdraw_amount)
 if balance < 0:
     print("Invalid balance")
-
 elif withdraw_amount <= 0:
     print("Invalid amount")
-
 elif withdraw_amount > balance:
     print("Insufficient balance")
-
 else:
     print("Remaining_balance : ", Remaining_balance) 
